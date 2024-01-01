78627335881f947574e1bde24aa8ea8d8b1d0823
ahmedMukhtar001 <kanzpanther@gmail.com>
2024-01-01 10:32:01 +0300

    save button function

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let countEl = document.getElementById('count-el');
-
-
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerHTML = count
-    console.log(count)
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count)
+    let dashSeparator = count + " - "
+    saveEl.textContent+= dashSeparator
+    countEl.textContent = 0
+    count = 0
 }
